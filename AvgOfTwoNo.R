7c7e82d4a775d02f16137e1f25963e9d4c2dcb5d
Anmol Kumar Singh <42077305+AnmolSingh-01@users.noreply.github.com>
2021-11-29 00:31:55 +0530

    Create AvgOfTwoNo.R

---
+++ AvgOfTwoNo.R
@@ -0,0 +1,6 @@
+x=as.integer(readline(prompt="Enter your first number"))
+y=as.integer(readline(prompt="Enter your second number"))
+print("Product of the two numbers")
+print(prod(x,y))
+print("Average of the two numbers")
+print(mean(x,y))
