272f9ee6ee83212cf4cb3d7ca5066d011ae0c8c2
Anmol Kumar Singh <42077305+AnmolSingh-01@users.noreply.github.com>
2021-11-29 00:46:16 +0530

    Create GreaterBetweenTwoNo.R

---
+++ GreaterBetweenTwoNo.R
@@ -0,0 +1,12 @@
+{
+  cat("Enter the first no : ")
+  a=scan(n=1)
+  cat("Enter the second no : ")
+  b=scan(n=1)
+  if(a>b){
+    print(paste(a,"is the greater than",b))
+  }else{
+    print(paste(b,"is greater than ",a))
+  }
+ 
+}
