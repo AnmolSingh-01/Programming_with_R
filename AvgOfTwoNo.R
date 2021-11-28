1064bc3ea4315f7012a4fdc5b5c8fe19b068e580
Anmol Kumar Singh <42077305+AnmolSingh-01@users.noreply.github.com>
2021-11-29 00:33:32 +0530

    Update AvgOfTwoNo.R

--- AvgOfTwoNo.R
+++ AvgOfTwoNo.R
@@ -1,5 +1,5 @@
-x=as.integer(readline(prompt="Enter your first number"))
-y=as.integer(readline(prompt="Enter your second number"))
+x=as.integer(readline(prompt="Enter your first number : "))
+y=as.integer(readline(prompt="Enter your second number :"))
 print("Product of the two numbers")
 print(prod(x,y))
 print("Average of the two numbers")
